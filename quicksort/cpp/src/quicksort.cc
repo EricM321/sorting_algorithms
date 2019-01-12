3bdf8dbe9d89e5e87f1f6e24acbfef0736d40007
ericm321 <ericm321@live.com>
2019-01-12 20:23:05 +0000

    Added more tests and functionality to quicksort file

--- quicksort/cpp/src/quicksort.cc
+++ quicksort/cpp/src/quicksort.cc
@@ -1,5 +1,33 @@
 #include <vector>
-
-std::vector<int> quicksort(std::vector<int> unsortedElements) {
-	return unsortedElements;
+void swap(int* a, int* b){
+	int t = *a;
+	*a = *b;
+	*b = t;
 }
+
+int partition(std::vector<int> arr, int low, int high){
+	int pivot = arr[high];
+	int i = (low - 1);
+	
+	for(int j = low; j <= high - 1; ++j){
+		if(arr[j] <= pivot){
+			++i;
+			swap(&arr[i], &arr[j]);
+		}
+	}
+	swap(&arr[i + 1], &arr[high]);
+	return (i + 1);
+}
+
+void quicksort(std::vector<int> unsorted_elements, int low, int high){
+	if(low < high){
+		int partitioning_index = partition(unsorted_elements, low, high);
+		quicksort(unsorted_elements, low, partitioning_index - 1);
+		quicksort(unsorted_elements, partitioning_index + 1, high);
+	}
+}
+
+std::vector<int> quicksort(std::vector<int> unsorted_elements) {
+	quicksort(unsorted_elements, 0, unsorted_elements.size());
+}
+
--- quicksort/cpp/test/tests.cc
+++ quicksort/cpp/test/tests.cc
@@ -2,11 +2,28 @@
 #include <vector>
 #include <gtest/gtest.h>
 
-TEST(QuicksortTest, PositiveNosArray) {
-	std::vector<int> unsortedNumbers = {2, 1, 3, 5, 4};
+TEST(SwapTest, Numbers){
+	std::vector<int> actual = {3};
+	std::vector<int> dummy = {2};
+	std::vector<int> expected = {2};
+	
+	swap(&actual[0], &dummy[0]);
+	ASSERT_EQ(expected, actual);
+}
+
+TEST(PartitionTest, Numbers){
+	std::vector<int> dummy = {6, 2, 5, 4, 1, 3};
+	int actual = partition(dummy, 0, 5);
+	
+	ASSERT_EQ(2, actual);
+}
+
+TEST(QuicksortTest, PositiveNos) {
+	std::vector<int> actual = {2, 1, 3, 5, 4};
 	std::vector<int> expected = {1, 2, 3, 4, 5};
 	
-    ASSERT_EQ(expected, quicksort(unsortedNumbers));
+	quicksort(actual);
+    ASSERT_EQ(expected, actual);
 }
 
 int main(int argc, char **argv) {
